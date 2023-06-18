9bd5a2a5e876b057deebffa3ec40e1db59f27221
Bernard Olivier <88403974+Bazz94@users.noreply.github.com>
2023-06-18 16:34:49 +0200

    Added more text & Minor fixes

--- ui/image-viewer.js
+++ ui/image-viewer.js
@@ -10,12 +10,17 @@ nextButton.addEventListener('click', function () {
   showSlides(slideIndex += 1);
 });
 
+let slides1 = document.getElementsByClassName("mySlides 1");
+let slides2 = document.getElementsByClassName("mySlides 2");
+let slides3 = document.getElementsByClassName("mySlides 3");
 let slideIndex = 1;
-showSlides(slideIndex);
+showSlides(slideIndex, slides1);
+showSlides(slideIndex, slides2);
+showSlides(slideIndex, slides3);
 
-function showSlides(n) {
+function showSlides(n, slides) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
+  
   if (n > slides.length) { slideIndex = 1 }
   if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
